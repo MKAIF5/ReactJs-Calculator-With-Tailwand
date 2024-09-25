0f3a1c9bb9cc0de9e67dceb5e3970b48f106f185
mohammed kaif <mkaif555123@gmail.com>
2024-09-25 20:16:54 +0500

    55% Work Completed

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,8 @@ function App() {
   return (
     <>
 
-      <div className='bg-white w-96 p-4 mx-auto my-24 rounded-xl'>
-
+      <div className='bg-white w-96 p-4 mx-auto my-8 rounded-xl'>
+        <h1 className='text-center text-2xl font-bold'>CALCULATOR</h1>
         <div className='flex justify-center my-5'>
           <input
             type="number"
@@ -16,33 +16,52 @@ function App() {
         </div>
         <br />
         <div className='flex justify-center gap-1 mb-2'>
-          <button className='p-4 bg-gray-400 border border-black text-black rounded-lg w-20 text-xl font-semibold'>AC</button>
-          <button className='p-4 bg-gray-400 border border-black text-black rounded-lg w-20 text-xl font-semibold'>DE</button>
-          <button className='p-4 bg-gray-400 border border-black text-black rounded-lg w-20 text-xl font-semibold'>.</button>
-          <button className='p-4 bg-gray-400 border border-black text-black rounded-lg w-20 text-xl font-semibold'>/</button>
+          <button className='p-4 bg-gray-400 border border-black text-black
+           rounded-lg w-20 text-xl font-semibold'>AC</button>
+          <button className='p-4 bg-gray-400 border border-black text-black
+           rounded-lg w-20 text-xl font-semibold'>DE</button>
+          <button className='p-4 bg-gray-400 border border-black text-black
+           rounded-lg w-20 text-xl font-semibold'>.</button>
+          <button className='p-4 bg-gray-400 border border-black text-black
+           rounded-lg w-20 text-xl font-semibold'>/</button>
         </div>
         <div className='flex justify-center gap-1 mb-2'>
-          <button className='p-4 bg-gray-400 border border-black text-black rounded-lg w-20 text-xl font-semibold'>7</button>
-          <button className='p-4 bg-gray-400 border border-black text-black rounded-lg w-20 text-xl font-semibold'>8</button>
-          <button className='p-4 bg-gray-400 border border-black text-black rounded-lg w-20 text-xl font-semibold'>9</button>
-          <button className='p-4 bg-gray-400 border border-black text-black rounded-lg w-20 text-xl font-semibold'>*</button>
+          <button className='p-4 bg-gray-400 border border-black text-black
+           rounded-lg w-20 text-xl font-semibold'>7</button>
+          <button className='p-4 bg-gray-400 border border-black text-black
+           rounded-lg w-20 text-xl font-semibold'>8</button>
+          <button className='p-4 bg-gray-400 border border-black text-black
+           rounded-lg w-20 text-xl font-semibold'>9</button>
+          <button className='p-4 bg-gray-400 border border-black text-black
+           rounded-lg w-20 text-xl font-semibold'>*</button>
         </div>
         <div className='flex justify-center gap-1 mb-2'>
-          <button className='p-4 bg-gray-400 border border-black text-black rounded-lg w-20 text-xl font-semibold'>4</button>
-          <button className='p-4 bg-gray-400 border border-black text-black rounded-lg w-20 text-xl font-semibold'>5</button>
-          <button className='p-4 bg-gray-400 border border-black text-black rounded-lg w-20 text-xl font-semibold'>6</button>
-          <button className='p-4 bg-gray-400 border border-black text-black rounded-lg w-20 text-xl font-semibold'>+</button>
+          <button className='p-4 bg-gray-400 border border-black text-black
+           rounded-lg w-20 text-xl font-semibold'>4</button>
+          <button className='p-4 bg-gray-400 border border-black text-black
+           rounded-lg w-20 text-xl font-semibold'>5</button>
+          <button className='p-4 bg-gray-400 border border-black text-black
+           rounded-lg w-20 text-xl font-semibold'>6</button>
+          <button className='p-4 bg-gray-400 border border-black text-black
+           rounded-lg w-20 text-xl font-semibold'>+</button>
         </div>
         <div className='flex justify-center gap-1 mb-2'>
-          <button className='p-4 bg-gray-400 border border-black text-black rounded-lg w-20 text-xl font-semibold'>1</button>
-          <button className='p-4 bg-gray-400 border border-black text-black rounded-lg w-20 text-xl font-semibold'>2</button>
-          <button className='p-4 bg-gray-400 border border-black text-black rounded-lg w-20 text-xl font-semibold'>3</button>
-          <button className='p-4 bg-gray-400 border border-black text-black rounded-lg w-20 text-xl font-semibold'>-</button>
+          <button className='p-4 bg-gray-400 border border-black text-black
+           rounded-lg w-20 text-xl font-semibold'>1</button>
+          <button className='p-4 bg-gray-400 border border-black text-black
+           rounded-lg w-20 text-xl font-semibold'>2</button>
+          <button className='p-4 bg-gray-400 border border-black text-black
+           rounded-lg w-20 text-xl font-semibold'>3</button>
+          <button className='p-4 bg-gray-400 border border-black text-black 
+          rounded-lg w-20 text-xl font-semibold'>-</button>
         </div>
         <div className='flex justify-center gap-1 mb-2'>
-          <button className='p-4 bg-gray-400 border border-black text-black rounded-lg w-20 text-xl font-semibold'>00</button>
-          <button className='p-4 bg-gray-400 border border-black text-black rounded-lg w-20 text-xl font-semibold'>0</button>
-          <button className='p-4 bg-gray-800 border border-black text-white rounded-lg w-40 text-xl font-semibold'>=</button>
+          <button className='p-4 bg-gray-400 border border-black text-black
+           rounded-lg w-20 text-xl font-semibold'>00</button>
+          <button className='p-4 bg-gray-400 border border-black text-black 
+          rounded-lg w-20 text-xl font-semibold'>0</button>
+          <button className='p-4 bg-gray-800 border border-black text-white
+           rounded-lg w-40 text-xl font-semibold'>=</button>
         </div>
       </div>
     </>
