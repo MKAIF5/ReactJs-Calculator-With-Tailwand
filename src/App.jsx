be4c6e2bdd7d1ced77b2a2b974d44e577e135c41
mohammed kaif <mkaif555123@gmail.com>
2024-09-25 19:37:32 +0500

    30% Work Completed

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,48 @@ function App() {
   return (
     <>
 
-      <div className='bg-white w-96 p-4 mx-auto my-40'>
+      <div className='bg-white w-96 p-4 mx-auto my-40 rounded-xl'>
 
         <div className='flex justify-center my-5'>
           <input
             type="text"
-            className='bg-gray-800 rounded-lg h-24 w-72 text-white p-1'
+            className='bg-gray-800 rounded-lg h-24 w-80 text-white p-1 text-right
+            text-3xl font-semibold outline-none'
           />
+        </div>
+        <br />
+        <div>
+          <button></button>
+          <button></button>
+          <button></button>
+          <button></button>
+        </div>
+        <div>
+          <button></button>
+          <button></button>
+          <button></button>
+          <button></button>
+        </div>
+        <div>
+          <button></button>
+          <button></button>
+          <button></button>
+          <button></button>
+
+        </div>
+        <div>
+          <button></button>
+          <button></button>
+          <button></button>
+          <button></button>
+
+        </div>
+        <div>
+          <button></button>
+          <button></button>
+          <button></button>
+          <button></button>
+
         </div>
       </div>
     </>
