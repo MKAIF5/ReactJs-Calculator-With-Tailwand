63131759152be046712fd8085ed74bf16d56a313
mohammed kaif <mkaif555123@gmail.com>
2024-09-25 19:16:38 +0500

    10% Work Completed

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,12 @@ function App() {
   return (
     <>
 
-      <div className='bg-white w-64 p-4 mx-auto my-40'>
+      <div className='bg-white w-96 p-4 mx-auto my-40'>
 
-        <div className='flex justify-center'>
+        <div className='flex justify-center my-5'>
           <input
             type="text"
-            className='bg-gray-800 rounded-lg'
+            className='bg-gray-800 rounded-lg h-24 w-72 text-white p-1'
           />
         </div>
       </div>
