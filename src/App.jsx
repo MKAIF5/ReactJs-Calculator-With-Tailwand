64b35fca9a1d7be9ff7280b9711628db2f0c82b3
mohammed kaif <mkaif555123@gmail.com>
2024-09-25 19:49:35 +0500

    40% Work Completed

--- src/App.jsx
+++ src/App.jsx
@@ -5,49 +5,23 @@ function App() {
   return (
     <>
 
-      <div className='bg-white w-96 p-4 mx-auto my-40 rounded-xl'>
+      <div className='bg-white w-96 p-4 mx-auto my-24 rounded-xl'>
 
         <div className='flex justify-center my-5'>
           <input
-            type="text"
+            type="number"
             className='bg-gray-800 rounded-lg h-24 w-80 text-white p-1 text-right
             text-3xl font-semibold outline-none'
           />
         </div>
         <br />
-        <div>
-          <button></button>
-          <button></button>
-          <button></button>
-          <button></button>
+        <div className='flex justify-center gap-1'>
+          <button className='p-4 bg-gray-400 border border-black text-black rounded-lg w-20 text-xl font-semibold'>AC</button>
+          <button className='p-4 bg-gray-400 border border-black text-black rounded-lg w-20 text-xl font-semibold'>DE</button>
+          <button className='p-4 bg-gray-400 border border-black text-black rounded-lg w-20 text-xl font-semibold'>.</button>
+          <button className='p-4 bg-gray-400 border border-black text-black rounded-lg w-20 text-xl font-semibold'>/</button>
         </div>
-        <div>
-          <button></button>
-          <button></button>
-          <button></button>
-          <button></button>
-        </div>
-        <div>
-          <button></button>
-          <button></button>
-          <button></button>
-          <button></button>
-
-        </div>
-        <div>
-          <button></button>
-          <button></button>
-          <button></button>
-          <button></button>
-
-        </div>
-        <div>
-          <button></button>
-          <button></button>
-          <button></button>
-          <button></button>
-
-        </div>
+       
       </div>
     </>
   )
